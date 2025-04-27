3cffd017961f2e1aee399a22f6ddffea07eb63e6
ZanatiZsolt <zzsolt05@gmail.com>
2025-04-28 01:04:50 +0200

    Web+

--- start.bat
+++ start.bat
@@ -1,12 +1,10 @@
 @echo off
 
-REM Háttérben indítja, nem nyit új ablakot
 cd BMZSWeb
-start /B npm  i && npm start
-
+start "BMZSWeb" cmd /k "npm i && npm start"
 cd ..
 
 
-REM Parancsikon futtatása ugyanebben a konzolban (de ez valójában külön folyamatként indul)
-start /B "" "ApiStarter.lnk"
+start "BMZSApi" ApiStarter.exe.lnk
+
 pause
